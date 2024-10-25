e80a02820f8b9afc236e313481ca1657fb97c48b
Travis Lawrence <travis.lawrence12@gmail.com>
2024-10-25 14:07:39 -0700

    working

--- src/extension.ts
+++ src/extension.ts
@@ -3,34 +3,41 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   const provider = vscode.languages.registerHoverProvider(
-    { scheme: "file", language: "javascript" }, // Specify the language
+    { scheme: "file", language: "javascript" },
     {
       provideHover(document, position, token) {
-        const wordRange = document.getWordRangeAtPosition(position, /'(.*?)'/);
+        const wordRange = document.getWordRangeAtPosition(
+          position,
+          /['"](.*?)['"]/
+        );
+
+        if (!wordRange) {
+          return;
+        }
+
         const hoveredWord = document.getText(wordRange);
         const jsonFilePath = path.join(__dirname, "data.json");
         try {
           const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, "utf8"));
-          const splitWord = hoveredWord.replaceAll("'", "").split(".");
+          const splitWord = hoveredWord.replace(/['"]/gi, "").split(".");
           const subtext = splitWord.reduce((word, param) => {
             return word[param];
           }, jsonData);
-          if (subtext) {
-            return new vscode.Hover(`**Description:** ${subtext.translation}`);
-          } else {
-            return new vscode.Hover(`No Text Found!`);
-          }
+
+          const output = subtext
+            ? `**Description:** ${subtext.translation}`
+            : "No text found!";
+          return new vscode.Hover(output);
         } catch (e) {
-          return new vscode.Hover(`ERROR!`);
+          console.log(e, "ERROR");
+          return null;
         }
       },
     }
   );
-
+  console.log("RUNNING");
   context.subscriptions.push(provider);
 }
 
---
+++ test.js
@@ -0,0 +1,7 @@
+const here = "AllWorkflowsPage.AllWorkflowsListEmptyState.Description";
+const here1 = "AllWorkflowsPage.AllWorkflowsListEmptyState.LearnMoreLink";
+const here2 =
+  "AllWorkflowsPage.AllWorkflowsListEmptyState.LearnMoreLinkAccessibilityText";
+const here3 =
+  "AllWorkflowsPage.AllWorkflowsListEmptyState.TutorialVideoModalTitle";
+const here4 = "AllWorkflowsPage.AllWorkflowsListEmptyState.ViewTutorialButton";
