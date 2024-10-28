e582aa96deeacc6dcfe23cb8f2f6acec4f77f079
Travis Lawrence <travis.lawrence12@gmail.com>
2024-10-28 11:02:38 -0700

    generally is working

--- src/searchProvider.ts
+++ src/searchProvider.ts
@@ -3,19 +3,52 @@ import * as vscode from "vscode";
 import { performSearch } from "./utils";
 
 export default (jsonFilePath: string) => {
-  return vscode.commands.registerCommand("extension.search", async () => {
-    const searchTerm = await vscode.window.showInputBox({
-      prompt: "Enter your search term",
-    });
+  return vscode.commands.registerCommand(
+    "extension.findAllSearch",
+    async () => {
+      const searchTerm = await vscode.window.showInputBox({
+        prompt: "Enter your search term",
+      });
 
-    if (searchTerm) {
-      const outputChannel = vscode.window.createOutputChannel("Search Results");
-      outputChannel.show();
+      if (searchTerm) {
+        const results = performSearch(searchTerm, jsonFilePath);
 
-      const results = performSearch(searchTerm, jsonFilePath);
+        console.log(results, "results");
 
-      outputChannel.appendLine(`Results for "${searchTerm}":`);
-      results.forEach((result) => outputChannel.appendLine(result));
+        await vscode.commands.executeCommand("workbench.action.findInFiles", {
+          query: results.join("|"),
+          isRegex: true,
+          triggerSearch: true,
+        });
+
+        // SEARCHES AND FINDS DIRECTLY IN A FILE
+        // let regex: RegExp;
+        // try {
+        //   regex = new RegExp(results.join("|"), "g");
+        // } catch (error) {
+        //   vscode.window.showErrorMessage("Invalid regex pattern.");
+        //   return;
+        // }
+        // const files = await vscode.workspace.findFiles("**/*");
+        // for (const file of files) {
+        //   const document = await vscode.workspace.openTextDocument(file);
+        //   const text = document.getText();
+        //   let match;
+
+        //   while ((match = regex.exec(text)) !== null) {
+        //     const position = document.positionAt(match.index);
+        //     const range = new vscode.Range(
+        //       position,
+        //       document.positionAt(match.index + match[0].length)
+        //     );
+
+        //     await vscode.window.showTextDocument(document).then((editor) => {
+        //       editor.revealRange(range, vscode.TextEditorRevealType.InCenter);
+        //       editor.selection = new vscode.Selection(range.start, range.end);
+        //     });
+        //   }
+        // }
+      }
     }
-  });
+  );
 };
