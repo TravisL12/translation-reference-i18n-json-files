427d25e344657fd2c079fca6d7519d86ace71bd2
Travis Lawrence <travis.lawrence12@gmail.com>
2024-10-28 11:07:42 -0700

    ok ready

--- src/searchProvider.ts
+++ src/searchProvider.ts
@@ -13,41 +13,11 @@ export default (jsonFilePath: string) => {
       if (searchTerm) {
         const results = performSearch(searchTerm, jsonFilePath);
 
-        console.log(results, "results");
-
         await vscode.commands.executeCommand("workbench.action.findInFiles", {
           query: results.join("|"),
           isRegex: true,
           triggerSearch: true,
         });
-
-        // SEARCHES AND FINDS DIRECTLY IN A FILE
-        // let regex: RegExp;
-        // try {
-        //   regex = new RegExp(results.join("|"), "g");
-        // } catch (error) {
-        //   vscode.window.showErrorMessage("Invalid regex pattern.");
-        //   return;
-        // }
-        // const files = await vscode.workspace.findFiles("**/*");
-        // for (const file of files) {
-        //   const document = await vscode.workspace.openTextDocument(file);
-        //   const text = document.getText();
-        //   let match;
-
-        //   while ((match = regex.exec(text)) !== null) {
-        //     const position = document.positionAt(match.index);
-        //     const range = new vscode.Range(
-        //       position,
-        //       document.positionAt(match.index + match[0].length)
-        //     );
-
-        //     await vscode.window.showTextDocument(document).then((editor) => {
-        //       editor.revealRange(range, vscode.TextEditorRevealType.InCenter);
-        //       editor.selection = new vscode.Selection(range.start, range.end);
-        //     });
-        //   }
-        // }
       }
     }
   );
